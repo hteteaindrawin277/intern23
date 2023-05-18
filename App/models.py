7eb682cb5432a9249eb9c31527514de3dda409fb
Htet Eain <hteteaindrawin7@gmail.com>
2023-05-18 13:48:15 +0630

    first

---
+++ App/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Info(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phnumber = models.CharField(max_length=15)
+    degree = models.CharField(max_length=20)
+
+# Create your models here.
